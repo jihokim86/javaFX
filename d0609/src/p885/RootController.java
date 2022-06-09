1d46f4c61b6ed589c4f9b0ab2571a81c58d2aa44
cadiablue1 <cadiablue1@naver.com>
2022-06-09 20:02:36 +0900

    22.06.09 : 이미지 넣기

---
+++ d0609/src/p885/RootController.java
@@ -0,0 +1,45 @@
+package p885;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class RootController implements Initializable {
+
+	@FXML private Button btn1; //private 생략가능 - default
+	@FXML private Button btn2;
+	@FXML private Button btn3;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		btn1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				handleBtn1Action(event);
+			}
+		});
+		
+		btn2.setOnAction(event->handleBtn2Action(event));
+		btn3.setOnAction(event->handleBtn3Action(event));
+		
+	}
+	
+	public void handleBtn1Action(ActionEvent event) {
+		System.out.println("버튼1 클릭");
+	}
+	
+	public void handleBtn2Action(ActionEvent event) {
+		System.out.println("버튼2 클릭");
+	}
+	
+	public void handleBtn3Action(ActionEvent event) {
+		System.out.println("버튼3 클릭");
+	}
+}
+
+
